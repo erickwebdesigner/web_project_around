24a319f4293040fc2e801533e4182cedfeb94a6c
erickwebdesigner <erickbarber10@gmail.com>
2024-08-07 19:07:16 -0300

    refactor: criação da classe formValidator e seu JS

--- script/index.js
+++ script/index.js
@@ -1,151 +1,164 @@
 import Card from './Card.js';
+import FormValidation from './validate.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-    const profbtn = document.querySelector('.profile__btn-info');
-    const formclose = document.querySelector('.profile__form-close');
-    const nameProfile = document.querySelector('.profile__name');
-    const jobProfile = document.querySelector('.profile__job');
-    const nameInput = document.querySelector('.profile__form-name');
-    const jobInput = document.querySelector('.profile__form-job');
-    const formEdit = document.querySelector('.profile__form');
+  const validationConfig = {
+      seletores: {
+          input: '.profile__input, .cards__input',
+          submitButton: '.profile__form-submit, .cards__form-submit'
+      },
+      classes: {
+          inactiveButton: 'profile__form-submit_disabled',
+          inputError: 'profile__input_type_error',
+          error: 'profile__error_visible'
+      }
+  };
 
-    // Inicializar os campos do perfil
-    nameProfile.textContent = 'Jaques Costeau';
-    jobProfile.textContent = 'Explorador';
+  // Inicializar a validação dos formulários
+  const profileFormValidator = new FormValidation(validationConfig, '#profile-form');
+  const cardFormValidator = new FormValidation(validationConfig, '#card-form');
 
-    function openForm() {
-        formEdit.classList.add('profile__form-open');
-    }
+  profileFormValidator.enableValidation();
+  cardFormValidator.enableValidation();
 
-    function closeForm() {
-        formEdit.classList.remove('profile__form-open');
-    }
+  // Restante do código para manipular os formulários e interações da página
+  const profbtn = document.querySelector('.profile__btn-info');
+  const formclose = document.querySelector('.profile__form-close');
+  const nameProfile = document.querySelector('.profile__name');
+  const jobProfile = document.querySelector('.profile__job');
+  const nameInput = document.querySelector('.profile__form-name');
+  const jobInput = document.querySelector('.profile__form-job');
+  const formEdit = document.querySelector('.profile__form');
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        // Atualizar os valores do perfil com os valores dos inputs
-        nameProfile.textContent = nameInput.value;
-        jobProfile.textContent = jobInput.value;
-        closeForm();
-    }
+  function openForm() {
+      formEdit.classList.add('profile__form-open');
+  }
 
-    profbtn.addEventListener('click', openForm);
-    formclose.addEventListener('click', closeForm);
-    formEdit.addEventListener('submit', handleSubmit);
+  function closeForm() {
+      formEdit.classList.remove('profile__form-open');
+  }
 
-    document.addEventListener('click', function(event) {
-        if (!formEdit.contains(event.target) && !profbtn.contains(event.target)) {
-            closeForm();
-        }
-    });
-
-    const profbtnplaces = document.querySelector('.profile__btn-card');
-    const cardsEdit = document.querySelector('.cards__form');
-    const formCardsclose = document.querySelector('.cards__form-close');
-    const cardsSection = document.querySelector('.cards');
-    const cardsName = document.querySelector('.cards__form-name');
-    const cardsLink = document.querySelector('.cards__form-link');
-
-    function cardsForm() {
-      cardsEdit.classList.toggle('cards__form-open');
-    }
-
-    function cardsFormclose() {
-      cardsEdit.classList.remove('cards__form-open');
-    }
-
-    function cardsHandleSubmit(event) {
+  function handleSubmit(event) {
       event.preventDefault();
-      const newCard = {
-        name: cardsName.value,
-        link: cardsLink.value
-      };
-      addCard(newCard);
-      cardsEdit.reset();
+      nameProfile.textContent = nameInput.value;
+      jobProfile.textContent = jobInput.value;
+      closeForm();
+  }
+
+  profbtn.addEventListener('click', openForm);
+  formclose.addEventListener('click', closeForm);
+  formEdit.addEventListener('submit', handleSubmit);
+
+  document.addEventListener('click', function(event) {
+      if (!formEdit.contains(event.target) && !profbtn.contains(event.target)) {
+          closeForm();
+      }
+  });
+
+  const profbtnplaces = document.querySelector('.profile__btn-card');
+  const cardsEdit = document.querySelector('.cards__form');
+  const formCardsclose = document.querySelector('.cards__form-close');
+  const cardsSection = document.querySelector('.cards');
+  const cardsName = document.querySelector('.cards__form-name');
+  const cardsLink = document.querySelector('.cards__form-link');
+
+  function cardsForm() {
+    cardsEdit.classList.toggle('cards__form-open');
+  }
+
+  function cardsFormclose() {
+    cardsEdit.classList.remove('cards__form-open');
+  }
+
+  function cardsHandleSubmit(event) {
+    event.preventDefault();
+    const newCard = {
+      name: cardsName.value,
+      link: cardsLink.value
+    };
+    addCard(newCard);
+    cardsEdit.reset();
+    cardsFormclose();
+  }
+
+  profbtnplaces.addEventListener('click', cardsForm);
+  formCardsclose.addEventListener('click', cardsFormclose);
+  cardsEdit.addEventListener('submit', cardsHandleSubmit);
+
+  document.addEventListener('click', function(event) {
+    if (!cardsEdit.contains(event.target) && !profbtnplaces.contains(event.target)) {
       cardsFormclose();
     }
+  });
 
-    profbtnplaces.addEventListener('click', cardsForm);
-    formCardsclose.addEventListener('click', cardsFormclose);
-    cardsEdit.addEventListener('submit', cardsHandleSubmit);
-
-    document.addEventListener('click', function(event) {
-      if (!cardsEdit.contains(event.target) && !profbtnplaces.contains(event.target)) {
-        cardsFormclose();
+  const initialCards = [
+      {
+          name: "Vale de Yosemite",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+      },
+      {
+          name: "Lago Louise",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+      },
+      {
+          name: "Montanhas Carecas",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+      },
+      {
+          name: "Latemar",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+      },
+      {
+          name: "Parque Nacional da Vanoise",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+      },
+      {
+          name: "Lago di Braies",
+          link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
       }
-    });
+  ];
 
+  initialCards.forEach(card => addCard(card));
 
+  function addCard(cardData) {
+      const card = new Card(cardData, '#card-template');
+      const cardElement = card.generateCard();
+      cardsSection.prepend(cardElement);
+  }
 
+  cardsSection.addEventListener('click', function(event) {
+      if (event.target.classList.contains('cards__image')) {
+          imageboxImage.setAttribute('src', event.target.src);
+          imageboxImage.setAttribute('alt', event.target.alt);
+          imageboxCaption.textContent = event.target.nextElementSibling.textContent;
+          imagebox.classList.add('imagebox__visible');
+      }
+  });
 
-    const initialCards = [
-        {
-            name: "Vale de Yosemite",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
-        },
-        {
-            name: "Lago Louise",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
-        },
-        {
-            name: "Montanhas Carecas",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
-        },
-        {
-            name: "Latemar",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
-        },
-        {
-            name: "Parque Nacional da Vanoise",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
-        },
-        {
-            name: "Lago di Braies",
-            link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-        }
-    ];
+  const imagebox = document.getElementById('imagebox');
+  const imageboxClose = document.getElementById('imagebox-close');
+  const imageboxImage = document.getElementById('imagebox-image');
+  const imageboxCaption = document.getElementById('imagebox-caption');
 
-    initialCards.forEach(card => addCard(card));
+  imageboxClose.addEventListener('click', function() {
+      imagebox.classList.remove('imagebox__visible');
+  });
 
-    function addCard(cardData) {
-        const card = new Card(cardData, '#card-template');
-        const cardElement = card.generateCard();
-        cardsSection.prepend(cardElement);
-    }
+  document.addEventListener('click', function(event) {
+      if (imagebox.classList.contains('imagebox__visible') && !imagebox.querySelector('.imagebox__content').contains(event.target) && !event.target.classList.contains('cards__image')) {
+          imagebox.classList.remove('imagebox__visible');
+      }
+  });
 
-    cardsSection.addEventListener('click', function(event) {
-        if (event.target.classList.contains('cards__image')) {
-            imageboxImage.setAttribute('src', event.target.src);
-            imageboxImage.setAttribute('alt', event.target.alt);
-            imageboxCaption.textContent = event.target.nextElementSibling.textContent;
-            imagebox.classList.add('imagebox__visible');
-        }
-    });
+  function keyHandler(event) {
+      if (event.key === 'Escape') {
+          closeForm();
+          cardsFormclose();
+          if (imagebox.classList.contains('imagebox__visible')) {
+              imagebox.classList.remove('imagebox__visible');
+          }
+      }
+  }
 
-    const imagebox = document.getElementById('imagebox');
-    const imageboxClose = document.getElementById('imagebox-close');
-    const imageboxImage = document.getElementById('imagebox-image');
-    const imageboxCaption = document.getElementById('imagebox-caption');
-
-    imageboxClose.addEventListener('click', function() {
-        imagebox.classList.remove('imagebox__visible');
-    });
-
-    document.addEventListener('click', function(event) {
-        if (imagebox.classList.contains('imagebox__visible') && !imagebox.querySelector('.imagebox__content').contains(event.target) && !event.target.classList.contains('cards__image')) {
-            imagebox.classList.remove('imagebox__visible');
-        }
-    });
-
-    function keyHandler(event) {
-        if (event.key === 'Escape') {
-            closeForm();
-            cardsFormclose();
-            if (imagebox.classList.contains('imagebox__visible')) {
-                imagebox.classList.remove('imagebox__visible');
-            }
-        }
-    }
-
-    document.addEventListener('keydown', keyHandler);
+  document.addEventListener('keydown', keyHandler);
 });
--- script/validate.js
+++ script/validate.js
@@ -1,86 +1,64 @@
-// Função para adicionar a mensagem de erro
-function addErrorMessage(tagElement, input) {
-  tagElement.textContent = input.validationMessage;
-}
-
-// Função para remover a mensagem de erro
-function removeErrorMessage(tagElement) {
-  tagElement.textContent = '';
-}
-
-// Função para adicionar a classe de erro
-function addErrorClass(input, errorClass) {
-  input.classList.add(errorClass);
-}
-
-// Função para remover a classe de erro
-function removeErrorClass(input, errorClass) {
-  input.classList.remove(errorClass);
-}
-
-// Função para validar os inputs
-function validateInputs(input, errorClass) {
-  const tagElement = input.nextElementSibling;
-  if (!input.validity.valid) {
-      addErrorMessage(tagElement, input);
-      addErrorClass(input, errorClass);
-  } else {
-      removeErrorMessage(tagElement);
-      removeErrorClass(input, errorClass);
+export default class FormValidation {
+  constructor({ seletores, classes }, formSelector) {
+    this._inputSelector = seletores.input;
+    this._submitButtonSelector = seletores.submitButton;
+    this._inactiveButtonClass = classes.inactiveButton;
+    this._inputErrorClass = classes.inputError;
+    this._errorClass = classes.error;
+    this._formElement = document.querySelector(formSelector);
+    this._inputList = Array.from(this._formElement.querySelectorAll(this._inputSelector));
+    this._buttonElement = this._formElement.querySelector(this._submitButtonSelector);
   }
-}
 
-// Função para verificar se algum input está inválido
-function hasInvalidInput(inputList) {
-  return inputList.some(input => !input.validity.valid);
-}
-
-// Função para alternar o estado do botão
-const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
-  if (hasInvalidInput(inputList)) {
-      buttonElement.classList.add(inactiveButtonClass);
-      buttonElement.disabled = true;
-  } else {
-      buttonElement.classList.remove(inactiveButtonClass);
-      buttonElement.disabled = false;
+  enableValidation() {
+    this._setEventListeners();
   }
-};
 
-// Função para habilitar a validação
-function enableValidation(elementos) {
-  const form = document.forms[elementos.formSelector];
-  const inputs = Array.from(form.querySelectorAll(elementos.inputsSelector));
-  const button = form.querySelector(elementos.buttomForm);
-
-  // Inicializa o estado do botão
-  toggleButtonState(inputs, button, elementos.buttonErrorClass);
-
-  // Adiciona evento de input para validação
-  for (const input of inputs) {
-      input.addEventListener('input', (event) => {
-          const element = event.target;
-          validateInputs(element, elementos.inputErrorClass);
-          toggleButtonState(inputs, button, elementos.buttonErrorClass);
+  _setEventListeners() {
+    this._toggleButtonState();
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
       });
+    });
+  }
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  _showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  _hideInputError(inputElement) {
+    const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
+    }
+  }
+
+  _hasInvalidInput() {
+    return this._inputList.some((inputElement) => !inputElement.validity.valid);
   }
 }
-
-// Habilitando a validação chamando enableValidation
-enableValidation({
-  formSelector: 'formProfile',
-  inputsSelector: '.profile__input',
-  buttomForm: '.profile__form-submit',
-  inputErrorClass: 'profile__form-error',
-  buttonErrorClass: 'profile-submit-disabled'
-});
-
-enableValidation({
-  formSelector: 'cardForm',  // Certifique-se de que o formSelector corresponda ao atributo 'name' do formulário
-  inputsSelector: '.cards__input',
-  buttomForm: '.cards__form-submit',
-  inputErrorClass: 'cards__form-error',
-  buttonErrorClass: 'card-submit-disabled'
-});
-
 
 
